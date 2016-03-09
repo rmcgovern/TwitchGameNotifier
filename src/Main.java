a659a03317254f6de7c626f086c16c200b770c56
rmcgovern <riley.mcgovern@yahoo.com>
2016-03-09 15:24:40 -0800

    Groundwork for GUI

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,30 @@
-import model.JSONReader;
+import java.io.IOException;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 
-    public static void main(String[] args) {
-        JSONReader.getGames();
-        JSONReader.getUserFollows("drtbikrider9");
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) {
+        Parent root;
+        try {
+            root = FXMLLoader.load(getClass().getResource("view/Main.fxml"));
+            Scene scene = new Scene(root, 700, 500);
+            
+            stage.setTitle("Twitch Game Notifier");
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
-
+    
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
