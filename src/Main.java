5942e19dcabb124d179f73531bc69ce1070e3e60
rmcgovern <riley.mcgovern@yahoo.com>
2016-03-09 16:34:31 -0800

    Beginnings of GUI

--- src/Main.java
+++ src/Main.java
@@ -3,21 +3,24 @@ import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 
 
 public class Main extends Application {
     @Override
-    public void start(Stage stage) {
-        Parent root;
+    public void start(Stage primaryStage) {
+        AnchorPane root;
         try {
-            root = FXMLLoader.load(getClass().getResource("view/Main.fxml"));
-            Scene scene = new Scene(root, 700, 500);
-            
-            stage.setTitle("Twitch Game Notifier");
-            stage.setScene(scene);
-            stage.show();
+         // Load root layout from fxml file.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Main.class.getResource("view/Main.fxml"));
+            root = (AnchorPane) loader.load();
+
+            // Show the scene containing the root layout.
+            Scene scene = new Scene(root);
+            primaryStage.setScene(scene);
+            primaryStage.show();
         } catch (IOException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
